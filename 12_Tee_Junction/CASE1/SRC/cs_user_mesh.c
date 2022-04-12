3c51c3c9d3957165fe08f282c5312b3b2f549aab
VINCENT STOBIAC <vincent.stobiac@edf.fr>
2022-04-12 17:15:34 +0200

    Tee_Junction: add study

---
+++ 12_Tee_Junction/CASE1/SRC/cs_user_mesh.c
@@ -0,0 +1,279 @@
+/*============================================================================
+ * Definition of the calculation mesh.
+ *
+ * Mesh-related user functions (called in this order):
+ *   1) Manage the exchange of data between code_saturne and the pre-processor
+ *   2) Define (conforming or non-conforming) mesh joinings.
+ *   3) Define (conforming or non-conforming) periodicity.
+ *   4) Define thin walls.
+ *   5) Modify the geometry and mesh.
+ *   6) Smoothe the mesh.
+ *============================================================================*/
+
+/* code_saturne version 7.2-alpha */
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2022 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+#include "cs_defs.h"
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+#include <assert.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*----------------------------------------------------------------------------
+ * Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_headers.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \file cs_user_mesh.c
+ *
+ * \brief Definition and modification of the calculation mesh.
+ *
+ * See \ref cs_user_mesh for examples.
+ */
+/*----------------------------------------------------------------------------*/
+
+/*============================================================================
+ * User function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Define mesh files to read and optional associated transformations.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_input(void)
+{
+
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Define mesh joinings.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_join(void)
+{
+
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Define periodic faces.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_periodicity(void)
+{
+
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Set options for cutting of warped faces.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_warping(void)
+{
+
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Insert boundaries into a mesh.
+ *
+ * \param[in,out] mesh  pointer to a cs_mesh_t structure
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_boundary(cs_mesh_t  *mesh)
+{
+  CS_UNUSED(mesh);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Modify geometry and mesh.
+ *
+ * \param[in,out] mesh  pointer to a cs_mesh_t structure
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_modify(cs_mesh_t  *mesh)
+{
+
+  {
+    /* Define extrusion parameters for each face */
+
+    int n_zones = 1;
+    const char *sel_criteria[] = {"walls"};
+    const int zone_layers[] = {4};
+    const double zone_thickness[] = {-1};
+    const float zone_expansion[] = {0.8};
+
+    cs_mesh_extrude_face_info_t *efi = cs_mesh_extrude_face_info_create(mesh);
+
+    cs_lnum_t n_faces;
+    cs_lnum_t *face_list;
+
+    BFT_MALLOC(face_list, mesh->n_b_faces, cs_lnum_t);
+
+    for (int z_id = 0; z_id < n_zones; z_id++) {
+
+      cs_selector_get_b_face_list(sel_criteria[z_id], &n_faces, face_list);
+
+      cs_mesh_extrude_set_info_by_zone(efi,
+                                       zone_layers[z_id],
+                                       zone_thickness[z_id],
+                                       zone_expansion[z_id],
+                                       n_faces,
+                                       face_list);
+    }
+
+  }
+
+  {
+    const char criteria[] = "box[-0.2, -0.1, -0.2, 0.2, 0.52, 0.15]";
+
+    cs_lnum_t   n_selected_cells = 0;
+    cs_lnum_t  *selected_cells = NULL;
+
+    BFT_MALLOC(selected_cells, mesh->n_cells, cs_lnum_t);
+
+    cs_selector_get_cell_list(criteria,
+                              &n_selected_cells,
+                              selected_cells);
+
+    cs_mesh_refine_simple_selected(mesh,
+                                   true,              /* conforming or not */
+                                   n_selected_cells,
+                                   selected_cells);
+
+    BFT_FREE(selected_cells);
+  }
+
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Mesh smoothing.
+ *
+ * \param[in,out] mesh  pointer to a cs_mesh_t structure
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_smoothe(cs_mesh_t  *mesh)
+{
+  CS_UNUSED(mesh);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Enable or disable mesh saving.
+ *
+ * By default, mesh is saved when modified.
+ *
+ * \param[in,out] mesh  pointer to a cs_mesh_t structure
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_save(cs_mesh_t  *mesh)
+{
+  CS_UNUSED(mesh);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Tag bad cells within the mesh based on user-defined geometric criteria.
+ *
+ * \param[in,out] mesh  pointer to a cs_mesh_t structure
+ * \param[in,out] mesh_quantities pointer to a cs_mesh_quantities_t structure
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_bad_cells_tag(cs_mesh_t             *mesh,
+                           cs_mesh_quantities_t  *mesh_quantities)
+{
+  CS_UNUSED(mesh);
+  CS_UNUSED(mesh_quantities);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Apply partial modifications to the mesh after the preprocessing
+ *        and initial postprocessing mesh building stage.
+ *
+ * \param[in,out] mesh  pointer to a cs_mesh_t structure
+ * \param[in,out] mesh_quantities pointer to a cs_mesh_quantities_t structure
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_modify_partial(cs_mesh_t             *mesh,
+                            cs_mesh_quantities_t  *mesh_quantities)
+{
+  CS_UNUSED(mesh);
+  CS_UNUSED(mesh_quantities);
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Define a cartesian mesh.
+*/
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_mesh_cartesian_define(void)
+{
+
+}
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
